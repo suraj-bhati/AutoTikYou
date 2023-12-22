312132ecd69b488b09e34215df3750166220deac
apk015 <127296145+apk015@users.noreply.github.com>
2023-12-22 09:25:31 +0530

    Update main.py v2.0

--- main.py
+++ main.py
@@ -25,17 +25,16 @@ def run_youtube_upload_script():
     print("Attempting to run youtube_upload.py")
     logging.info("Attempting to run youtube_upload.py")
     try:
-        os.system("python3 youtube_upload.py")
-        print("Successfully ran youtube_upload.py")
-        logging.info("Successfully ran youtube_upload.py")
+        exit_code = os.system("python3 youtube_upload.py")
+        return exit_code == 0  # True if upload was successful, False otherwise
     except Exception as e:
         print(f"Error in youtube_upload.py: {e}")
         logging.error(f"Error in youtube_upload.py: {e}")
         return False
-    print("Waiting 10 seconds after youtube_upload.py")
-    logging.info("Waiting 10 seconds after youtube_upload.py")
-    time.sleep(10)  # 10-second delay after running youtube_upload.py
-    return True
+    finally:
+        print("Waiting 10 seconds after youtube_upload.py")
+        logging.info("Waiting 10 seconds after youtube_upload.py")
+        time.sleep(10)  # 10-second delay after running youtube_upload.py
 
 def update_urls_csv():
     print("Attempting to update urls.csv")
@@ -44,7 +43,6 @@ def update_urls_csv():
         with open("urls.csv", "r") as file:
             lines = file.readlines()
         if lines:
-            # Remove the first line (processed URL)
             lines.pop(0)
             with open("urls.csv", "w") as file:
                 file.writelines(lines)
@@ -59,6 +57,9 @@ def update_urls_csv():
 
 def main():
     loop_counter = 0
+    successful_uploads = 0
+    skipped_videos = 0
+
     while True:
         loop_counter += 1
         print(f"Starting Loop {loop_counter}")
@@ -66,19 +67,26 @@ def main():
 
         if run_get_data_script():
             if os.path.exists("data.json"):
-                if run_youtube_upload_script():
-                    print("Deleting data.json file")
-                    logging.info("Deleting data.json file")
-                    os.remove("data.json")
-                    update_urls_csv()
+                upload_success = run_youtube_upload_script()
+                print("Deleting data.json file")
+                logging.info("Deleting data.json file")
+                os.remove("data.json")
+                update_urls_csv()  # Always remove URL after processing
+
+                if upload_success:
+                    successful_uploads += 1
                     print("Waiting 10 seconds after updating urls.csv")
                     logging.info("Waiting 10 seconds after updating urls.csv")
                     time.sleep(10)  # 10-second delay after updating urls.csv
 
-        waiting_time = 21600  # 6 hours in seconds
-        print(f"Completed Loop {loop_counter}. Waiting for {waiting_time / 3600} hours before the next iteration.")
-        logging.info(f"Completed Loop {loop_counter}. Waiting for {waiting_time / 3600} hours before the next iteration.")
-        time.sleep(waiting_time)  # Wait for 6 hours
+                    waiting_time = 21600  # 6 hours in seconds
+                    print(f"Completed Loop {loop_counter}. Waiting for {waiting_time / 3600} hours before the next iteration.")
+                    print(f"Total successful uploads: {successful_uploads}, Skipped videos: {skipped_videos}")
+                    logging.info(f"Completed Loop {loop_counter}. Waiting for {waiting_time / 3600} hours before the next iteration.")
+                    logging.info(f"Total successful uploads: {successful_uploads}, Skipped videos: {skipped_videos}")
+                    time.sleep(waiting_time)  # Wait for 6 hours
+                else:
+                    skipped_videos += 1
 
 if __name__ == "__main__":
     main()
